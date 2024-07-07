220a19e6f3105c406ac76a435eeb12df38c46e45
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-07-07 15:00:02 -0400

    initialized the project and connected to the mongodb

---
+++ server/config/connection.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://katvkaiser:bMb45QF51pnIYNac@mern-shopping.ojp1lj1.mongodb.net/test?retryWrites=true&w=majority&appName=mern-shopping');
+
+module.exports = mongoose.connection;
